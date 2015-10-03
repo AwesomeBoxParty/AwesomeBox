264a1eb691f9942dba2e2a5067d42f7566e4eb22
JP Ji <baka101@gmail.com>
2015-10-03 15:55:26 -0700

    connect server and client side code.  Updated package.json

--- Server/Server.js
+++ Server/Server.js
@@ -1,5 +1,6 @@
 // SETUP:
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var request = require('request');
@@ -8,20 +9,20 @@ var socket = require('socket.io')(http);
 
 // OBJECTS:
 var SongManager = require("./SongManager.js").SongManager;
-var songManager = new SongManagaer();
+var songManager = new SongManager();
 var User = require("./User.js").User;
 
 // MEMORY:
+var PORT = process.env.PORT || 3020;
 var users = [];
 
 // SETUP:
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended : false
+	extended : true
 }));
-app.configure(function() {
-	app.use(express.static(path.join(__dirname, 'public')));
-});
+
+app.use(express.static(path.join(__dirname, '../dist')));
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
@@ -62,3 +63,11 @@ var publishSongs = function() {
 }
 
 setEventHandlers();
+
+app.listen(PORT, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://localhost:' + PORT);
+});
