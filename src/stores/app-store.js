adbfe661328d58f763338228f0499e4ce178f16e
Tamara <tmwoodson@gmail.com>
2015-10-04 16:00:54 -0700

    Hook up flux

--- src/stores/app-store.js
+++ src/stores/app-store.js
@@ -4,6 +4,7 @@ var assign = require("react/lib/Object.assign");
 var EventEmitter = require('events').EventEmitter;
 
 var _songs = [];
+var _partyThrower = false;
 
 
 var _receiveSongData = function (data) {
@@ -15,20 +16,32 @@ var _otherFunction = function () {
 
 };
 
+var _receiveUserRole = function(data) {
+  console.log('Calling _receiveUserRole in AppStore:', data);
+  _partyThrower = data;
+};
+
 var AppStore = assign({},EventEmitter.prototype, {
 
   getSongs: function() {
     return _songs;
   },
 
+  getUserRole: function() {
+    console.log('getUserRole being called in AppStore');
+    return _partyThrower;
+  },
+
   /////////////////////////////////////////////////
   // DATA STORE FUNCTIONS
   /////////////////////////////////////////////////
   emitChange: function() {
+    console.log('Emitting change');
     this.emit('change');
   },
 
   addChangeListener: function(callback) {
+    console.log('change listener added:', callback);
     this.on('change', callback);
   },
 
@@ -41,7 +54,7 @@ var AppStore = assign({},EventEmitter.prototype, {
 
 
 AppStore.dispatchToken = AppDispatcher.register(function(action) {
-  
+
   switch(action.type) {
 
     case AppConstants.LOAD_SONG_DATA:
@@ -49,6 +62,13 @@ AppStore.dispatchToken = AppDispatcher.register(function(action) {
       AppStore.emitChange();
       break;
 
+    case AppConstants.RECEIVE_USER_ROLE:
+      console.log('App store: receiving user role -- ', action.data);
+      _receiveUserRole(action.data);
+      AppStore.emitChange();
+      break;
+
+    default:
   }
 });
 
